7f81d840d66cf40771079246502450ea49e3e713
ajoanos <sklep@allemedia.pl>
2025-09-26 13:42:55 +0200

    Delete allemedia-wedding-timeline directory

--- allemedia-wedding-timeline/includes/class-deactivator.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Dezaktywacja wtyczki - brak destrukcyjnych operacji (zostawiamy dane).
- */
-class Allemedia_WT_Deactivator {
-    public static function deactivate() {
-        // Na razie brak akcji - zachowujemy tabele, by nie utracić harmonogramów.
-    }
-}
